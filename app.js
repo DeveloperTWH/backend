8a6dc739dc636b0aaa37bfd7e89adad91d9fda12
DeveloperTWH <webdevelopment.twh@gmail.com>
2025-06-16 17:31:10 +0530

    16-06-2025 cors made according to render

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const cors = require('cors');
 const userRoutes = require('./routes/userRoutes')
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',  // your frontend URL
+  credentials: true                 // optional, only if using cookies or auth headers
+}));
+
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
